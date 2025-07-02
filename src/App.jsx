95a37e7f7f60613122574c994676462f07350e5d
Taysir-Darwish <taiseerdarwish@gmail.com>
2025-07-03 00:57:13 +0300

    add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components
+import Navbar from "./components/common/Nav";
+import Footer from "./components/common/Footer";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Navbar /> 
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+      </Routes>
+
+      <Footer /> 
+    </Router>
+  );
 }
 
-export default App
+export default App;
