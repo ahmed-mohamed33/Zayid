c1b994fbc05b0541b817e2bcf1c89e9836f9f090
Taysir-Darwish <taiseerdarwish@gmail.com>
2025-08-09 05:30:16 +0300

    fix pug when refresh after EndAuction

--- src/components/auction/BiddingChat.jsx
+++ src/components/auction/BiddingChat.jsx
@@ -102,7 +102,6 @@ const BiddingChat = ({
         } else {
           setRemainingTime("0:00:00");
           setIsAuctionLive(true);
-          updateAuctionStatus(auctionId, "active");
         }
       };
 
