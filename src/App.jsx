e52b2babdbbf1ea5ef8d4198d19b6963f67a0712
Taysir-Darwish <taiseerdarwish@gmail.com>
2025-08-12 07:43:15 +0300

    Add a title to the pages + the name of the auction

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import Profile from "./Pages/Profile.jsx";
 import Dashboard from "./Pages/Dashboard.jsx";
 import FAq from "./Pages/FAq.jsx";
 import ErrorPage from "./components/common/errorPage.jsx";
+import TitleUpdater from "./components/common/TitleUpdater.jsx";
 
 function App() {
   // Initialize service worker for notifications
@@ -51,6 +52,7 @@ function App() {
         <Router>
           <Navbar />
           <AllDataComponent />
+          <TitleUpdater/>
           <Routes>
             {/* Public Routes */}
             <Route path="/" element={<HomePage />} />
